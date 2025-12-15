8c56877d2a4dd3c7fddd1095a2f40449f4ffe799
Krampst <krautkramerhenry@gmail.com>
2025-12-15 15:14:33 -0800

    g

--- Scripts/Worlds/wow.gd
+++ Scripts/Worlds/wow.gd
@@ -1,17 +1,19 @@
 extends Node2D
+var lost  =false
 func win():
-	$CharacterBody2D/Sprite2D.play("WOW")
-	$WinAnimation.active = true
+	if !lost:
+		$CharacterBody2D/Sprite2D.play("WOW")
+		$WinAnimation.play("WOWAnimation")
+		$objScroller_spawner.queue_free()
 func lose():
+	lost = true
+	$objScroller_spawner.queue_free()
 	
-	
-	print("loss")
 func start():
 	$objScroller_spawner.on = true
 	$Timer.start()
 func _ready() -> void:
 	$CharacterBody2D.whereToSendLose = self
-	$WinAnimation.active = false
 	G.CurrentGame = self
 	$CharacterBody2D/Sprite2D.play("default")
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
