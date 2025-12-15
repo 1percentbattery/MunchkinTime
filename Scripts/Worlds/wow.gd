6732904c4e4460ecce77a3bcd089502cc02c2de3
Krampst <krautkramerhenry@gmail.com>
2025-12-15 15:49:42 -0800

    gh

--- Scripts/Worlds/wow.gd
+++ Scripts/Worlds/wow.gd
@@ -1,22 +1,24 @@
 extends Node2D
+var lost  =false
 func win():
-	
-	$CharacterBody2D/Sprite2D.play("WOW")
-	$WinAnimation.active = true
-	G.SceneSwitcher.Score += 1
-	await get_tree().create_timer(3.0).timeout
-	G.SceneSwitcher.switch()
+	if !lost:
+		$CharacterBody2D/Sprite2D.play("WOW")
+		$WinAnimation.play("WOWAnimation")
+		$objScroller_spawner.queue_free()
 func lose():
-	
-	
-	print("loss")
+	lost = true
+	$CharacterBody2D/Sprite2D.play("WOW")
+	$objScroller_spawner.queue_free()
+	$LightDefaultSquare.material.set_shader_parameter("add",Vector4(0,-1,-1,0))
+	$loseAnimation.play("WOWyoulost")
 func start():
 	$objScroller_spawner.on = true
 	$Timer.start()
+
 func _ready() -> void:
 	$CharacterBody2D.whereToSendLose = self
-	$WinAnimation.active = false
-	G.CurrentGame = self
 	$CharacterBody2D/Sprite2D.play("default")
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
+func _on_lose_animation_animation_finished(anim_name: StringName) -> void:
 	pass # Replace with function body.
