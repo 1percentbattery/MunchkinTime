e7a1bcd333d6abf89dddc6391ea13a6d544f8817
Krampst <krautkramerhenry@gmail.com>
2025-12-16 12:54:16 -0800

    g

--- Scripts/Worlds/frogger.gd
+++ Scripts/Worlds/frogger.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 func _ready():
 	$Timer.hide()
+	$WinBackground2.hide()
 func start():
 	#await get_tree().create_timer(1.0).timeout
 	$CharacterBody2D.on = true
@@ -9,13 +10,23 @@ func start():
 	$Timer.start()
 func win():
 	$Timer.hide()
+	$Spawners.queue_free()
+	$decoration.queue_free()
+	$WinBox.queue_free()
+	$CharacterBody2D.position = Vector2(960,540)
 	
-	G.wonGame()
 func lose():
+	$Spawners.set_process_mode(ProcessMode.PROCESS_MODE_DISABLED)
+	$CharacterBody2D.set_process_mode(ProcessMode.PROCESS_MODE_DISABLED)
+	$WinBackground2.show()
 	$Timer.hide()
-	
-	G.lostGame()
+	$AnimationPlayer.play("Loser")
 func _on_win_box_body_entered(body: Node2D) -> void:
 	win()
 func _on_win_box_area_entered(area: Area2D) -> void:
 	win()
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Loser":
+		G.lostGame()
+	else:
+		G.wonGame()
