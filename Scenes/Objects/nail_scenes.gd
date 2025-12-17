2003d8411c41de6e497b4375845a07ebba26403c
Krampst <krautkramerhenry@gmail.com>
2025-12-17 12:54:09 -0800

    f

--- Scenes/Objects/nail_scenes.gd
+++ Scenes/Objects/nail_scenes.gd
@@ -1,12 +1,17 @@
 extends Node2D
 @export var timePerNail : float = 0.5
 @onready var boardList = [$Boards/Board1,$Boards/Board2,$Boards/Board3,$Boards/Board4]
+@onready var displayList = [$Boards/Board1/Numbers1,$Boards/Board2/Numbers2,$Boards/Board3/Numbers3,$Boards/Board4/Numbers4]
+@onready var hammer = $Hammer
 var order = []
 var numList = [0,1,2,3]
-
+@export var amnt = 0.0
+@export var on = false
 func _ready():
 	for board in boardList:
 		board.play("default")
+	for x in displayList:
+		x.play("default")
 	for x in randi_range(1,4):
 		var ran = numList.pick_random()
 		order += [ran]
@@ -14,5 +19,47 @@ func _ready():
 	for x in order.size():
 		print(order[x])
 		boardList[order[x]].play("Nail")
-		
-	
+		displayList[order[x]].play(str(x))
+		#order.size() * timePerNail
+func start():
+	$Timer.waitTime = timePerNail
+	$Timer.start()
+	on = true
+func _physics_process(delta: float) -> void:
+	$LightDefaultSquare.material.set_shader_parameter("amnt",amnt)
+	if !on:
+		return
+	var directionHit : int = -1
+	if Input.is_action_just_pressed("Up"):
+		directionHit = 0
+	if Input.is_action_just_pressed("Down"):
+		directionHit = 2
+	if Input.is_action_just_pressed("Left"):
+		directionHit = 1
+	if Input.is_action_just_pressed("Right"):
+		directionHit = 3
+	if directionHit >=0 :
+		hammer.position=boardList[directionHit].position+Vector2(72,-63)
+		$HamerHit.play("HammerHit")
+	else:
+		return
+	if order[0] == directionHit:
+		boardList[order[0]].play("Won")
+		order.pop_front()
+		if order.is_empty():
+			win()
+			return
+	else:
+		if order.has(directionHit):
+			pass
+		else:
+			boardList[directionHit].play("Lost")
+		lose()
+func win():
+	on = false
+	$Win.play("Win")
+func _on_win_animation_finished(anim_name: StringName) -> void:
+	get_parent().win()
+func lose():
+	on = false
+	get_parent().lose()
--- Scripts/Worlds/HammerGame.gd
+++ Scripts/Worlds/HammerGame.gd
@@ -1,22 +1,30 @@
 extends Node2D
 class_name HammerGameScript
-@export var timePerNail : float = 0.5
+@export var timePerNail : float = 3.0
 @export var goal : int = 3
-var nailScene : PackedScene
+var nailScene : PackedScene = load("res://Scenes/Objects/nail_scenes.tscn")
 var curScene : Node2D = null
-var curPoints : int = 0
+var curPoints : int = -1
+var on = false
 func _ready():
-	
-	pass
+	nextScene()
+	start()
+func start():
+	on = true
+	curScene.start()
 func nextScene():
-	if curScene:
-		curScene.queue_free()
-	var tempObj: Node2D = nailScene.instantiate()
-	add_child(tempObj)
-	curScene = tempObj
 	curPoints += 1
 	if curPoints >= goal:
 		win()
+		return
+	if curScene:
+		curScene.queue_free()
+	var tempObj: Node2D = nailScene.instantiate()
+	tempObj.timePerNail = timePerNail
+	add_child(tempObj)
+	curScene = tempObj
+	if on:
+		curScene.start()
 func win():
 	pass
 func lose():
